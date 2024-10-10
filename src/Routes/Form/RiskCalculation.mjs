f5f114a64e5c7336da9568643556684e0e5d21e7
adfou <adfouberr@gmail.Com>
2024-10-10 06:32:32 +0200

    push

--- src/Routes/Form/RiskCalculation.mjs
+++ src/Routes/Form/RiskCalculation.mjs
@@ -27,6 +27,7 @@ export const RiskCalculation = async (req, res) => {
         if (form) {
             // If the form exists, return the form data
             //console.log(form.form_data)
+            console.log("form.form_data:",form.form_data)
             const dataObj = JSON.parse(form.form_data);
             //console.log(dataObj["general-information"])
             const AcitvityCalcul = AcitvityCalculation(dataObj?.["your-physical-activity"])
